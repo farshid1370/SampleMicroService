bd643248b8b0d9cc081251d437595dc492d1f338
Farshid Amirkhani <farshid.amirkhani@gmail.com>
2023-01-28 19:42:49 +0330

    remove grpc for client requests

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -16,32 +16,24 @@ builder.WebHost.ConfigureKestrel(options =>
 builder.Services.AddGrpc(options =>
 {
     options.EnableDetailedErrors = true;
-  
-}).AddJsonTranscoding();
-builder.Services.AddGrpcSwagger();
+
+});
+
 builder.Services.AddSwaggerGen(c =>
 {
     c.SwaggerDoc("v1",
         new OpenApiInfo { Title = "Catalog gRPC", Version = "v1" });
 });
-builder.Services.AddGrpcReflection();
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("CorsPolicy",
-        builder => builder
-            .SetIsOriginAllowed((host) => true)
-            .AllowAnyMethod()
-            .AllowAnyHeader()
-            .AllowCredentials());
-});
+
+
 builder.Services.AddMediatR(Assembly.GetExecutingAssembly());
 
 builder.Services.RegisterInfrastructureServices();
 
 var app = builder.Build();
-app.UseCors("CorsPolicy");
-app.MapGrpcReflectionService();
-// Configure the HTTP request pipeline.
+
+
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
